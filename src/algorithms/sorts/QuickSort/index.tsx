6452ee3a5f386ae692eb51c2f888c1d378e7c26f
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-07-29 18:39:01 +0700

    qsort works fine, but count comprasions is fail..

--- src/algorithms/sorts/QuickSort/index.tsx
+++ src/algorithms/sorts/QuickSort/index.tsx
@@ -7,7 +7,8 @@ import { quickSortFirstWrapper } from './quickSortCoursera';
 // const arr = getRandomStringArray(210);
 
 import { data } from './data';
-const arr = data.filter((item, index) => index <= 20);
+const arr = data.filter((item, index) => index <= 400000);
+// const arr = data;
 // const arr = ['3', '8', '2', '5', '1', '4', '7', '6'];
 // const arr = ['5', '4', '1', '6', '3', '2', '9'];
 // const arr = ['2', '4', '3', '5'];
@@ -20,49 +21,63 @@ type State = {
 
 export class QuickSort extends React.PureComponent<Props, State> {
   getStyle() {
-    const checkTrue = {
+    const base = {
+      display: 'inline-block',
+      padding: '0 5px',
+    };
+    const cellInit = {
+      ...base
+    };
+    const cellTrue = {
+      ...base,
       color: 'green'
     };
-    const checkFalse = {
+    const cellFalse = {
+      ...base,
+      fontWeight: 'bold' as 'bold',
       color: 'red'
     };
     return {
-      checkTrue,
-      checkFalse,
+      cellInit,
+      cellTrue,
+      cellFalse,
     };
   }
-  //const style = this.getStyle();
+
   render() {
     const style = this.getStyle();
-    const sortedArray1 = quickSortFirstWrapper([...arr]);
-    // quickSortFirstWrapper(arr);
-    // quick_sort(arr);
-    /*
-    const sortedArray2 = quick_sort([...arr]);
-
-    <div>
-      {sortedArray2.join(' ')}
-    </div>
-    */
-    let check = true;
-    for (let i = 1; i < sortedArray1.length; i++) {
-      if (sortedArray1[i - 1] > sortedArray1[i]) {
-        check = false;
-      }
-    }
+    const sortedArray = quickSortFirstWrapper([...arr]);
+    const valid = this.isValid(sortedArray);
 
     return (
       <div>
         <h3>Array</h3>
-        {arr.join(' ')}
+        {arr.map(item => <span key={item} style={style.cellInit}>{item}</span>)}
+
         <h3>Sorted</h3>
         <div>
-          {sortedArray1.join(' ')}
+          {sortedArray.map((item, index, array) => <span key={index} style={this.getSortedItemStyle(array, index, style)}>{item}</span>)}
         </div>
         <div>
-          <h3 style={check ? style.checkTrue : style.checkFalse}>Check is {String(check)}</h3>
+          <h5 style={valid ? style.cellTrue : style.cellFalse}>Check is {String(valid)}</h5>
         </div>
       </div>
     );
+  }
+
+  getSortedItemStyle(array: string[], index: number, style: any) {
+    if (index === 0) {
+      return style.cellTrue;
+    }
+    return this.cmpWithPrev(array, index) ? style.cellTrue : style.cellFalse;
+  }
+
+  isValid(sortedArray: string[]) {
+    let check = true;
+    for (let i = 1; i < sortedArray.length; i++) { if (Number(sortedArray[i - 1]) > Number(sortedArray[i])) { check = false } }
+    return check;
+  }
+  cmpWithPrev(arr: string[], index: number) {
+    return Number(arr[index - 1]) <= Number(arr[index]);
   }
 }
--- src/algorithms/sorts/QuickSort/quickSortCoursera.ts
+++ src/algorithms/sorts/QuickSort/quickSortCoursera.ts
@@ -1,16 +1,17 @@
 import { swap } from './utils';
 
 // import { overflowStopper, swap } from './utils';
-// let countOfRecursiveSteps = 0;
+let countOfRecursiveSteps = 0;
+let countOfRecursiveSteps2 = 0;
 
 const getPivot = (arr: string[], start: number) => {
   return arr[start];
 };
 
 const partition = (arr: string[], start: number, end: number) => {
-  console.log(`PARTITION START start=${start}, end=${end} now, arr=`, arr);
+  // console.log(`PARTITION START start=${start}, end=${end} now, arr=`, arr);
   const pivot = getPivot(arr, start);
-  console.log(`PARTITION PIVOT,`, pivot);
+  // console.log(`PARTITION PIVOT,`, pivot);
   if (pivot !== arr[start]) {
     swap(arr, pivot, arr[start]);
   }
@@ -22,58 +23,62 @@ const partition = (arr: string[], start: number, end: number) => {
   while (k < end) {
 
     if (Number(arr[k]) < Number(pivot)) {
+      countOfRecursiveSteps++;
       // console.log(`this is pre part, i=${i}, j=${j}, k=${k}, arr[k]=${arr[k]}`);
 
       // console.log(`swap i=${i}, j=${j}, arr[i] = ${arr[i]}, arr[j] = ${arr[j]}, arr[k] = ${arr[k]}`);
       swap(arr, arr[i], arr[k]);
 
       i++;
-      k++;
+      // k++;
       // console.log(`now, arr=`, arr);
     }
 
-    if (Number(arr[k]) > Number(pivot)) {
-      // console.log(`this is after part, i=${i}, j=${j}, k=${k}, arr[k]=${arr[k]}`);
-      // j++;
-      k++;
-      // console.log(`now, arr=`, arr);
-    }
+    // if (Number(arr[k]) > Number(pivot)) {
+    // countOfRecursiveSteps++;
+    // console.log(`this is after part, i=${i}, j=${j}, k=${k}, arr[k]=${arr[k]}`);
+    // j++;
+    k++;
+    // console.log(`now, arr=`, arr);
+    // }
   }
 
   // console.log(`swap last`);
   swap(arr, arr[i - 1], arr[start]);
-  console.log(`PARTITION DONE start=${start}, end=${end} now, arr=`, arr);
+  // console.log(`PARTITION DONE start=${start}, end=${end} now, arr=`, arr);
 
 
   /* ****************************************** */
   return pivot;
 };
 
-let blocker = 0;
+// let blocker = 0;
 
 export const qsort = (arr: string[], start: number, end: number): string[] => {
   if ((end - start) <= 1) {
     return arr;
   }
-  if (blocker >= 10) {
-    console.log('EXIT BY BLOCKER');
-    return arr;
-  }
-  blocker++;
+  // if (blocker >= 4000) {
+  //   // console.log('EXIT BY BLOCKER');
+  //   return arr;
+  // }
+  // blocker++;
 
   // return arr;
 
   const pivot = partition(arr, start, end);
   const pivotIndex = arr.indexOf(pivot);
 
-  console.log(`start left side, start=${start}, end=${pivotIndex + 1}`);
-  qsort(arr, start, pivotIndex + 1);
-  console.log(`start left side, start=${start}, end=${pivotIndex + 1}, done`);
+  // console.log(`START LEFT SIDE, START=${start}, END=${pivotIndex}`, arr[start], arr[pivotIndex]);
+  countOfRecursiveSteps2 = countOfRecursiveSteps2 + pivotIndex - start - 1;
+  qsort(arr, start, pivotIndex);
+  // console.log(`START LEFT SIDE, START=${start}, END=${pivotIndex}, DONE`, arr[start], arr[pivotIndex]);
 
 
-  console.log(`start right side, start=${pivotIndex + 1}, end=${end}`);
+  // console.log(`START RIGHT SIDE, START=${pivotIndex + 1}, END=${end}`, arr[pivotIndex + 1], arr[end - 1]);
+  countOfRecursiveSteps2 = countOfRecursiveSteps2 + end - (pivotIndex + 1) - 1;
   qsort(arr, pivotIndex + 1, end);
-  console.log(`start right side, start=${pivotIndex + 1}, end=${end}, done`);
+  // console.log(`START RIGHT SIDE, START=${pivotIndex + 1}, END=${end}, DONE`, arr[pivotIndex + 1], arr[end - 1]);
 
   // const arrLeft = pivotIndex === 0 ? [pivot] : arr.slice(0, pivotIndex);
   // const arrRight = arr.slice(pivotIndex, arr.length - 1);
@@ -96,9 +101,11 @@ export const qsort = (arr: string[], start: number, end: number): string[] => {
 
 
 export const quickSortFirstWrapper = (arr: string[]) => {
+  countOfRecursiveSteps2 = arr.length - 1;
   const res = qsort(arr, 0, arr.length);
-  // console.log('myFirst: countOfRecursiveSteps', countOfRecursiveSteps);
-  console.log('DONE', res);
+  console.log('myFirst: countOfRecursiveSteps', countOfRecursiveSteps);
+  console.log('myFirst: countOfRecursiveSteps2', countOfRecursiveSteps2);
+  // console.log('DONE', res);
 
   return res;
 };
