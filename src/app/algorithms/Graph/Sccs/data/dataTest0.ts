9a3a7db66fc61b04fedcabf8be8761bf3ee8a6f5
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-08-16 17:12:17 +0700

    sccs

--- src/app/algorithms/Graph/Sccs/data/dataTest0.ts
+++ src/app/algorithms/Graph/Sccs/data/dataTest0.ts
@@ -3,8 +3,8 @@ export const data =
 1 2
 2 3
 3 4
-4 5
 4 9
+4 5
 5 6
 6 7
 7 8
--- src/app/algorithms/Graph/Sccs/index.tsx
+++ src/app/algorithms/Graph/Sccs/index.tsx
@@ -4,7 +4,7 @@ import * as R from 'ramda';
 // import { data as dataR } from './data/data';
 // import { data } from './data/dataTest0';
 // import { data } from './data/dataTest9';
-import { data } from './data/dataTest0';
+import { data } from './data/dataTest00';
 import { sccs } from './sccs';
 
 type Props = {
@@ -30,7 +30,12 @@ export class Sccs extends React.PureComponent<Props, State> {
       <div>
         <h3>SCCs - Strongly Connected Components</h3>
         <h5>{R.split(/(\}, \{)|(\[\{)|(\}\])/, result).map(
-          (item: any, index: number) => <div key={index}>{item}</div>
+          (item: any, index: number) => {
+            if (!item || item.indexOf('len') === -1) {
+              return null;
+            }
+            return <div key={index}>{item}</div>;
+          }
         )}</h5>
       </div>
     );
