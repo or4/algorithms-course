11ab4f7c6c5d397ec769e8206cabcd9e579c8050
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-08-21 12:49:03 +0700

    dijkstra before refactoring

--- src/app/algorithms/Graph/Dijkstra/dijkstra.ts
+++ src/app/algorithms/Graph/Dijkstra/dijkstra.ts
@@ -1,13 +1,39 @@
 import * as R from 'ramda';
-import { convertToArray } from './utils';
+import { convertToArray, getMinVerticeEdge, getMinVertice } from './utils';
 import { objToString } from '../../../ramda/helpers';
 
 export const runDijkstra = (raw: string, vertice: number) => {
   const graph = convertToArray(raw);
   // console.log('graph', R.pipe(R.toPairs, R.map(R.map(objToString)), R.fromPairs)(graph));
   console.log('graph', graph);
+  let node;
+  let minEdge;
+  let weightFromCurrNode;
+  let weightFromAnotherNode;
+  let locVertice = vertice;
 
-  const node = graph[vertice];
-  console.log('node', objToString(node));
+  node = graph[locVertice];
+  node.value = 0;
 
+  while (locVertice !== -1) {
+    node = graph[locVertice];
+    for (let i = 0; i < node.edges.length; i++) {
+      minEdge = getMinVerticeEdge(node.edges);
+      if (!graph[minEdge.vertice].done) {
+        weightFromCurrNode = node.value + minEdge.weight;
+        weightFromAnotherNode = graph[minEdge.vertice].value;
+        if (weightFromCurrNode < weightFromAnotherNode) {
+          graph[minEdge.vertice].value = weightFromCurrNode;
+        }
+      }
+      minEdge.visited = true;
+    }
+    node.done = true;
+
+    locVertice = getMinVertice(graph, locVertice);
+    console.log('locVertice', locVertice);
+  }
+
+  console.log('graph', graph);
 };
+
--- src/app/algorithms/Graph/Dijkstra/utils.ts
+++ src/app/algorithms/Graph/Dijkstra/utils.ts
@@ -3,12 +3,14 @@ import * as R from 'ramda';
 type VerticeNode = {
   vertice: number;
   weight: number;
+  visited: boolean;
 };
 
 export type Graph = {
   [key: number]: {
     edges: VerticeNode[];
     value: number;
+    done: boolean;
   };
 };
 
@@ -21,14 +23,44 @@ export const convertToArray = (data: string): Graph => {
     R.mapObjIndexed(
       R.pipe(
         R.drop(1), // skip first element (this is not item, this is a vertice that key object)
-        R.map<any, VerticeNode[]>( // transform item to object like { vertice: 12, weight: 17 }
-          R.pipe<string, string[], number[], VerticeNode>(R.split(','), R.map(Number), R.zipObj(['vertice', 'weight']) as any)
+        R.map<any, any[]>( // transform item to object like { vertice: 12, weight: 17 }
+          R.pipe<string, string[], number[], any, any>(R.split(','), R.map(Number), R.zipObj(['vertice', 'weight']) as any, R.assoc('visited', false) as any)
         ),
         R.objOf('edges'), // tranform to object with key edges and values VerticeNode[]
-        R.assoc('value', 0)
+        R.assoc('value', Infinity),
+        R.assoc('done', false),
       ),
     ),
   )(data) as any;
 
   return splitted;
+};
+
+
+export const getMinVerticeEdge = (nodes: VerticeNode[]): VerticeNode => {
+  return R.pipe<VerticeNode[], VerticeNode[], VerticeNode>(
+    R.sortWith([
+      R.ascend(R.prop('visited')),
+      R.ascend(R.prop('weight'))
+    ]),
+    R.head
+  )(nodes) as VerticeNode;
+};
+
+
+export const getMinVertice = (graph: Graph, vertice: number): number => {
+
+  const verticeNode = graph[vertice];
+  const edges = R.sortWith([
+    R.ascend(R.prop('weight'))
+  ])(verticeNode.edges) as VerticeNode[];
+
+  for (let i = 0; i < edges.length; i++) {
+    const subVertice = graph[edges[i].vertice];
+    if (!subVertice.done) {
+      return edges[i].vertice;
+    }
+  }
+
+  return -1;
 };
