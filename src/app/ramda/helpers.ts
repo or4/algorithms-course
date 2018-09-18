c0e9443436136220d3d0e6692db25449bfcd92a9
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-09-18 10:46:40 +0700

    refactor objToString

--- src/app/ramda/helpers.ts
+++ src/app/ramda/helpers.ts
@@ -1,63 +1,62 @@
 import * as R from 'ramda';
 
-// this code is very not efficiency, only debug
-// cause objToString is recursive function
-// and after each call function pipe is created
-// doesnt work with cyclic links aka child -> parent -> child
-// export function objToString(obj: object): string {
-//   return R.pipe(
-//     R.toPairs,
-//     R.map(
-//       R.ifElse(
-//         item => typeof item[1] === 'object',
-//         (item: any[]) => `${item[0]}:  ${objToString(item[1])}`,
-//         // (item: any[]) => `${item[0]}:  ${item[1]}`,
-//         R.join(': ')
-//       )
-//     ),
-//     R.join(', '),
-//     R.concat('{ '),
-//     R.partialRight(R.concat, [' }'])
-//   )(obj) as any;
-// }
+export const isNotNil = R.complement(R.isNil);
 
-export function objToString(value: any): any {
-  if (
-    typeof value === 'number' ||
-    typeof value === 'string' ||
-    typeof value === 'undefined' ||
-    typeof value === 'boolean'
-  ) {
+export function notEquals(arg1: any, arg2: any): boolean {
+  return R.complement(R.equals)(arg1, arg2);
+}
+
+let currentMaxDeep: number;
+
+const convertArray = R.pipe<any[], string[], string, string, string>(
+  R.map((item) => toString(item)),
+  R.join(', '),
+  R.concat('['),
+  R.partialRight(R.concat as any, [']'])
+);
+
+const convertObject = R.pipe(
+  R.toPairs,
+  R.map(
+    R.ifElse(
+      item => typeof item[1] === 'object', // value object recursive call function
+      (item: any[]) => `${item[0]}: ${toString(item[1])}`,
+      R.join(': ')
+    )
+  ),
+  R.join(', '),
+  R.concat('{ '),
+  R.partialRight(R.concat, [' }'])
+);
+
+// not working if exists cycles
+function toString(value: any): string {
+  if (R.equals(currentMaxDeep, 0)) {
+    return String(value);
+  }
+  currentMaxDeep--;
+
+  if (R.equals(typeof value, 'function')) {
+    return String(value);
+  }
+  if (notEquals(typeof value, 'object')) {
     return value;
   }
 
   if (Array.isArray(value)) {
-    return R.pipe<any[], any[], string, string, string>(
-      R.map((item) => objToString(item)) as any,
-      R.join(', '),
-      R.concat('['),
-      R.partialRight(R.concat as any, [']'])
-    )(value);
-
-    // return '[' + value.map(
-    //   item => objToString(item)
-    // ).join(', ') + ']';
+    return convertArray(value);
   }
 
-  return R.pipe(
-    R.toPairs,
-    R.map(
-      R.ifElse(
-        item => typeof item[1] === 'object',
-        (item: any[]) => `${item[0]}: ${objToString(item[1])}`,
-        R.join(': ')
-      )
-    ),
-    R.join(', '),
-    R.concat(Array.isArray(value) ? '[ ' : '{ '),
-    R.partialRight(R.concat, [Array.isArray(value) ? ' ]' : ' }'])
-  )(value) as any;
+  return convertObject(value) as any;
 }
 
+export function objToString(value: any, maxDeep: number = 5): string {
+  currentMaxDeep = maxDeep; // this dirty hack need to save perfomance and prevent stack overflow when cycles
+  try {
+    return toString(value);
+  }
+  catch (err) {
+    return `Error when converting to string: ${err}, ${String(value)}`;
+  }
+}
 
-export const isNotNil = R.complement(R.isNil);
