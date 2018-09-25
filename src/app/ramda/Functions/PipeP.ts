b6699057a4335c5e2f6c6996b607eef42ec1bbaa
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-09-25 17:30:26 +0700

    pipeP

--- src/app/ramda/Functions/PipeP.ts
+++ src/app/ramda/Functions/PipeP.ts
@@ -15,3 +15,24 @@
 // //   )),
 // //   R.fromPairs
 // // )(worklogs);
+
+
+// export async function addProjectInfo(worklogs: DataType): Promise<DataType> {
+//   const result = R.pipeP(
+//     async w => R.toPairs(w),
+//     async w => Promise.all(R.map(async ([key, value]) => [key, value * await timeout()], w)),
+//     async w => R.fromPairs(w),
+//   )({ a: 123, b: 444, c: 555 });
+
+//   console.log('result', await result);
+
+//   return worklogs;
+// }
+
+  // return Promise.all(
+  //   R.map(
+  //     async ([projectKey, projectValue]) => [projectKey, await getProjectWithInfo(projectKey, projectValue)],
+  //     R.toPairs(worklogs)
+  //   )
+  // ).then(R.fromPairs);
+}
