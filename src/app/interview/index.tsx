82f289723d8da0b7d82abcec5561a934a34dd61e
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-09-28 12:37:52 +0700

    TextOverflowEllipsis

--- src/app/interview/index.tsx
+++ src/app/interview/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { Column31 } from './css/Column31';
 import { Column32 } from './css/Column32';
+import { TextOverflowEllipsis } from './css/TextOverflowEllipsis';
 
 
 type Props = {
@@ -18,11 +19,13 @@ export class Interview extends React.PureComponent<Props, State> {
         <div>
           <Link to={`/interview/css/column31`}>Column31</Link>
           <Link to={`/interview/css/column32`}>Column32</Link>
+          <Link to={`/interview/css/textOverflowEllipsis`}>TextOverflowEllipsis</Link>
         </div>
 
         <Switch>
           <Route path="/interview/css/column31" component={Column31} />
           <Route path="/interview/css/column32" component={Column32} />
+          <Route path="/interview/css/textOverflowEllipsis" component={TextOverflowEllipsis} />
         </Switch>
       </div>
     );
